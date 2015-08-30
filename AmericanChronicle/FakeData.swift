9761039560fde97240201c5cf486441ac795bfbb
Ryan Peterson <ryan@carbonfive.com>
2015-08-29 19:51:13 -0700

    Deleted all sample page images (cleanup in progress).

---
+++ AmericanChronicle/FakeData.swift
@@ -0,0 +1,11 @@
+//
+//  FakeData.swift
+//  AmericanChronicle
+//
+//  Created by Ryan Peterson on 8/29/15.
+//  Copyright (c) 2015 ryanipete. All rights reserved.
+//
+
+enum LargePageImageName: String {
+    case Generic = ""
+}
