61084059d1e23700da3019e9212a8f4898352c8b
Ryan Peterson <ryan@carbonfive.com>
2015-09-05 16:15:33 -0700

    WIP

--- AmericanChronicle/HomeViewController.swift
+++ AmericanChronicle/HomeViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 class HomeViewController: UITableViewController, UITextFieldDelegate, UIViewControllerTransitioningDelegate {
 
     
--- AmericanChronicle/NewspaperPagesViewController.swift
+++ AmericanChronicle/NewspaperPagesViewController.swift
@@ -8,39 +8,138 @@
 
 import UIKit
 
-class NewspaperPagesViewController: UIViewController, UICollectionViewDelegate, NewspaperPagesPreviewActionHandler, UIViewControllerTransitioningDelegate {
+class TransitioningDelegate: NSObject, UIViewControllerTransitioningDelegate {
+
+    var percentComplete: CGFloat {
+        get {
+            return focusTransition.percentComplete
+        }
+        set {
+            focusTransition.updateInteractiveTransition(newValue)
+        }
+    }
+
+    func cancelOrFinish() {
+        if percentComplete > 0.5 {
+            println("will call finishInteractiveTransition")
+            focusTransition.finishInteractiveTransition()
+            println("did call finishInteractiveTransition")
+        } else {
+            println("will call cancelInteractiveTransition")
+            focusTransition.cancelInteractiveTransition()
+            println("did call cancelInteractiveTransition")
+        }
+    }
+
+    private var focusTransition = InteractivePageFocusTransition()
+
+    // MARK: UIViewControllerTransitioningDelegate methods
+
+    func animationControllerForPresentedController(
+            presented: UIViewController,
+            presentingController presenting: UIViewController,
+            sourceController source: UIViewController
+        ) -> UIViewControllerAnimatedTransitioning? {
+        return focusTransition
+    }
+
+    //func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    //}
+
+    func interactionControllerForPresentation(
+            animator: UIViewControllerAnimatedTransitioning
+        ) -> UIViewControllerInteractiveTransitioning? {
+
+        println("Returning \(focusTransition) as interactionControllerForPresentation")
+        return focusTransition
+    }
+    
+//    func interactionControllerForDismissal(animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+//    }
+
+    //func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController!, sourceViewController source: UIViewController) -> UIPresentationController? {
+    //}
+    
+}
+
+class InteractivePageFocusTransition: UIPercentDrivenInteractiveTransition, UIViewControllerAnimatedTransitioning {
+    let transitionDuration = 2.0
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        println("transitionContext \(transitionContext.description)")
+        let toView = transitionContext.viewForKey(UITransitionContextToViewKey)
+        if let toView = toView {
+            transitionContext.containerView().addSubview(toView)
+        }
+        UIView.animateWithDuration(transitionDuration, animations: {
+            toView?.frame = CGRectOffset(toView?.frame ?? CGRectZero, 200.0, 0)
+        }, completion: { finished in
+            println("Animation completion block called. Finished? \(finished)")
+            println(" * transitionWasCancelled(): \(transitionContext.transitionWasCancelled())")
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+        })
+    }
+
+    func animationEnded(transitionCompleted: Bool) {
+        println("animationEnded called. Transition completed? \(transitionCompleted)")
+    }
+
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        return transitionDuration
+    }
+}
+
+//class InteractivePageUnfocusTransition: UIPercentDrivenInteractiveTransition, UIViewControllerAnimatedTransitioning {
+//
+//}
+
+class NewspaperPagesViewController: UIViewController, UICollectionViewDelegate, NewspaperPagesPreviewActionHandler {
 
     @IBOutlet weak var previewCollectionView: UICollectionView!
     @IBOutlet weak var stripCollectionView: UICollectionView!
     @IBOutlet var previewDelegate: NewspaperPagesPreviewDelegate!
     @IBOutlet var dataSource: NewspaperPagesDataSource!
+
+
+    let pageFocusTransitioningDelegate = TransitioningDelegate()
+
     var issue: NewspaperIssue? {
         didSet {
             if !isViewLoaded() {
                 return
             }
-            println("\(__FILE__) | \(__FUNCTION__) | line \(__LINE__)")
             previewDelegate.issue = issue
         }
     }
 
-    var focusTransition: PageFocusTransition?
+    func presentPage() {
+        let sb = UIStoryboard(name: "Page", bundle: nil)
+        if let vc = sb.instantiateInitialViewController() as? PageViewController {
+            vc.imageName = issue?.imageName
+            vc.transitioningDelegate = pageFocusTransitioningDelegate
+            vc.modalTransitionStyle = UIModalTransitionStyle(rawValue: UIModalPresentationStyle.Custom.rawValue)!
+            vc.doneCallback = { [weak self] in
+                self?.dismissViewControllerAnimated(true, completion: nil)
+            }
+            println("Will call presentViewController...")
+            presentViewController(vc, animated: true, completion: nil)
+            println("Did call presentViewController...")
+        }
+    }
 
     @IBAction func pinchRecognized(sender: UIPinchGestureRecognizer) {
-        if let transition = focusTransition {
-            transition.progress = sender.scale - 1.0
-        } else {
-            focusTransition = PageFocusTransition()
-        let sb = UIStoryboard(name: "Page", bundle: nil)
-            if let vc = sb.instantiateInitialViewController() as? PageViewController {
-                vc.imageName = issue?.imageName
-                vc.transitioningDelegate = self
-                vc.modalPresentationStyle = .Custom
-                vc.doneCallback = { [weak self] in
-                    self?.dismissViewControllerAnimated(true, completion: nil)
-                }
-                presentViewController(vc, animated: true, completion: nil)
-            }
+
+        switch sender.state {
+        case .Possible:
+            println("Possible")
+        case .Began:
+            println("Pinch began")
+            presentPage()
+
+        case .Changed:
+            println("Changed")
+            pageFocusTransitioningDelegate.percentComplete = sender.scale - 1.0
+        default: // Ended, Cancelled, Failed
+            pageFocusTransitioningDelegate.cancelOrFinish()
         }
     }
 
@@ -80,181 +179,5 @@ class NewspaperPagesViewController: UIViewController, UICollectionViewDelegate, 
             }
         }
     }
-
-    // MARK: UIViewControllerTransitioningDelegate methods
-
-//    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-//        return PageFocusTransition()
-//    }
-//    class PageFocusTransition: NSObject, UIViewControllerAnimatedTransitioning {
-//        let duration = 1.0
-//        func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-//            let fromVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)
-//            let toVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)
-//            if let pagesViewController = fromVC as? NewspaperPagesViewController,
-//                   pageViewController = toVC as? PageViewController {
-//
-//                var presentingVC: UIViewController? = pagesViewController
-//                while !(presentingVC?.definesPresentationContext ?? false) {
-//                    presentingVC = presentingVC?.parentViewController
-//                }
-//
-//                println("presentingVC: \(presentingVC)")
-//
-//                let fullScreenImage = imageOfWindow()
-//
-//                fullScreenImageView.image = fullScreenImage
-//                fullScreenImageView.frame = CGRect(x: 0, y: 0, width: fullScreenImage.size.width, height: fullScreenImage.size.height)
-//
-//                navBarImageView.image = fullScreenImage
-//                navBarImageView.frame = CGRect(x: 0, y: 0, width: fullScreenImage.size.width, height: 64.0)
-//
-//                pageViewController.prepareForAppearanceAnimation()
-//
-//                presentingVC?.view.addSubview(fullScreenImageView)
-//
-//                presentingVC?.view.addSubview(navBarCoverView)
-//
-//                presentingVC?.view.addSubview(pageViewController.view)
-//
-//                presentingVC?.view.addSubview(navBarImageView)
-//
-//                pageViewController.presentingView = fullScreenImageView
-//                pageViewController.presentingViewNavBar = navBarImageView
-//
-//                UIView.animateWithDuration(2.0, animations: {
-//                    pageViewController.updateViewsInsideAppearanceAnimation()
-//                    self.navBarImageView.frame = CGRectOffset(self.navBarImageView.frame, 0, -64.0)
-//                    }, completion: { _ in
-//                        self.navBarImageView.removeFromSuperview()
-//                        self.navBarCoverView.removeFromSuperview()
-//                        self.fullScreenImageView.removeFromSuperview()
-//                        pagesViewController.presentViewController(pageViewController, animated: false, completion: nil)
-//                })
-//            }
-//        }
-//
-//        func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
-//            return duration
-//        }
-//    }
-
-
-    //func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-    //}
-
-    func interactionControllerForPresentation(animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-        return focusTransition
-    }
-
-    class PageFocusTransition: NSObject, UIViewControllerInteractiveTransitioning {
-
-        var context: UIViewControllerContextTransitioning?
-        var progress: CGFloat = 0 {
-            didSet {
-                println("progress: \(progress)")
-                // update progress
-//                pageViewController.updateViewsInsideAppearanceAnimation()
-                let offset = -64.0 * progress
-                var navBarFrame = self.navBarImageView.frame
-                navBarFrame.origin.y = offset
-                self.navBarImageView.frame = navBarFrame
-                context?.updateInteractiveTransition(progress)
-                if progress >= 1.0 {
-                    println("\(__FILE__) | \(__FUNCTION__) | line \(__LINE__)")
-                    context?.completeTransition(true)
-                }
-            }
-        }
-
-        private func imageOfWindow() -> UIImage {
-            let window = UIApplication.sharedApplication().keyWindow!
-            UIGraphicsBeginImageContext(window.bounds.size)
-            window.drawViewHierarchyInRect(window.bounds, afterScreenUpdates: false)
-            let fullScreenImage = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            return fullScreenImage
-        }
-
-        var navBarCoverView: UIView = {
-            let navBarCoverView = UIView()
-            navBarCoverView.backgroundColor = UIColor.blackColor()
-            return navBarCoverView
-            }()
-
-        var fullScreenImageView: UIImageView = {
-            return UIImageView()
-            }()
-
-        var navBarImageView: UIImageView = {
-            let navBarImageView = UIImageView()
-            navBarImageView.clipsToBounds = true
-            navBarImageView.contentMode = .Top
-            return navBarImageView
-            }()
-
-        func startInteractiveTransition(transitionContext: UIViewControllerContextTransitioning) {
-            context = transitionContext
-            let fromVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)
-            let toVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)
-
-            if let pagesViewController = fromVC as? NewspaperPagesViewController,
-                   pageViewController = toVC as? PageViewController {
-
-                    var presentingVC: UIViewController? = pagesViewController
-
-                    while !(presentingVC?.definesPresentationContext ?? false) {
-                        presentingVC = presentingVC?.parentViewController
-                    }
-
-                    let fullScreenImage = imageOfWindow()
-
-//                    fullScreenImageView.image = fullScreenImage
-                    fullScreenImageView.frame = CGRect(x: 0, y: 0, width: fullScreenImage.size.width, height: fullScreenImage.size.height)
-                    fullScreenImageView.backgroundColor = UIColor.greenColor()
-
-                    navBarImageView.image = fullScreenImage
-                    navBarImageView.frame = CGRect(x: 0, y: 0, width: fullScreenImage.size.width, height: 64.0)
-
-//                    pageViewController.prepareForAppearanceAnimation()
-
-                    presentingVC?.view.addSubview(fullScreenImageView)
-//
-//                    presentingVC?.view.addSubview(navBarCoverView)
-//
-//                    presentingVC?.view.addSubview(pageViewController.view)
-//
-//                    presentingVC?.view.addSubview(navBarImageView)
-//
-//                    pageViewController.presentingView = fullScreenImageView
-//                    pageViewController.presentingViewNavBar = navBarImageView
-
-//                    UIView.animateWithDuration(2.0, animations: {
-//                        pageViewController.updateViewsInsideAppearanceAnimation()
-//                        self.navBarImageView.frame = CGRectOffset(self.navBarImageView.frame, 0, -64.0)
-//                        }, completion: { _ in
-//                            self.navBarImageView.removeFromSuperview()
-//                            self.navBarCoverView.removeFromSuperview()
-//                            self.fullScreenImageView.removeFromSuperview()
-//                            pagesViewController.presentViewController(pageViewController, animated: false, completion: nil)
-//                    })
-            }
-
-        }
-
-//        func completionSpeed() -> CGFloat {
-//
-//        }
-//        func completionCurve() -> UIViewAnimationCurve {
-//
-//        }
-    }
-
-    //func interactionControllerForDismissal(animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-    //}
-
-    //func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController!, sourceViewController source: UIViewController) -> UIPresentationController? {
-    //}
-
 }
 
