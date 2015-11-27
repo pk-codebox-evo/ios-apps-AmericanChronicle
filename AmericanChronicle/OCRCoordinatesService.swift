80c8aa9ccadd219fcce5bf7ee74b5e1112420372
Ryan Peterson <ryan@carbonfive.com>
2015-11-26 18:30:44 -0800

    Filled out OCRCoordinatesService.

--- AmericanChronicle/OCRCoordinatesService.swift
+++ AmericanChronicle/OCRCoordinatesService.swift
@@ -12,7 +12,94 @@ import Alamofire
 // MARK: OCRCoordinatesServiceInterface
 
 public protocol OCRCoordinatesServiceInterface {
-    func startRequest(lccn: String, year: Int, month: Int, day: Int, edition: String, sequence: String, contextID: String, completionHandler: ((SearchResults?, ErrorType?) -> Void))
-    func isSearchInProgress(term: String, page: Int, contextID: String) -> Bool
-    func cancelSearch(term: String, page: Int, contextID: String)
+    func startRequest(
+            lccn: String,
+            date: NSDate,
+            edition: String,
+            sequence: String,
+            contextID: String,
+            completionHandler: ((OCRCoordinates?, ErrorType?) -> Void))
+    func isRequestInProgress(
+            lccn: String,
+            date: NSDate,
+            edition: String,
+            sequence: String,
+            contextID: String) -> Bool
+    func cancelRequest(
+            lccn: String,
+            date: NSDate,
+            edition: String,
+            sequence: String,
+            contextID: String)
+}
+
+public class OCRCoordinatesService: OCRCoordinatesServiceInterface {
+
+    private let manager: ManagerProtocol
+    private var activeRequests: [String: RequestProtocol] = [:]
+    private let queue = dispatch_queue_create("com.ryanipete.AmericanChronicle.OCRCoordinatesService", DISPATCH_QUEUE_SERIAL)
+
+    public init(manager: ManagerProtocol = Manager()) {
+        self.manager = manager
+    }
+
+    public func startRequest(
+            lccn: String,
+            date: NSDate,
+            edition: String,
+            sequence: String,
+            contextID: String,
+            completionHandler: ((OCRCoordinates?, ErrorType?) -> Void))
+    {
+        if lccn.characters.count <= 0 {
+            completionHandler(nil, NSError(code: .InvalidParameter, message: "Tried to fetch OCR info using an empty lccn."))
+            return
+        }
+
+        if isRequestInProgress(lccn, date: date, edition: edition, sequence: sequence, contextID: contextID) {
+            completionHandler(nil, NSError(code: .DuplicateRequest, message: "Message tried to send a duplicate request."))
+            return
+        }
+
+
+        let URLString = URLStringForLCCN(lccn, date: date, edition: edition, sequence: sequence)
+
+        let request = self.manager.request(.GET, URLString: URLString, parameters: nil)?.responseObject { (obj: OCRCoordinates?, error) in
+            dispatch_sync(self.queue) {
+                self.activeRequests[URLString] = nil
+            }
+            completionHandler(obj, error)
+        }
+
+        dispatch_sync(queue) {
+            self.activeRequests[URLString] = request
+        }
+    }
+
+    private func URLStringForLCCN(lccn: String, date: NSDate, edition: String, sequence: String) -> String {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let dateString = formatter.stringFromDate(date)
+        return "lccn/\(lccn)/\(dateString)/\(edition)/\(sequence)/coordinates/"
+    }
+
+    public func isRequestInProgress(
+            lccn: String,
+            date: NSDate,
+            edition: String,
+            sequence: String,
+            contextID: String) -> Bool
+    {
+        return activeRequests[URLStringForLCCN(lccn, date: date, edition: edition, sequence: sequence)] != nil
+    }
+
+    public func cancelRequest(
+            lccn: String,
+            date: NSDate,
+            edition: String,
+            sequence: String,
+            contextID: String)
+    {
+
+    }
 }
---
+++ AmericanChronicleTests/OCRCoordinatesServiceTests.swift
@@ -0,0 +1,86 @@
+//
+//  OCRCoordinatesServiceTests.swift
+//  AmericanChronicle
+//
+//  Created by Ryan Peterson on 11/22/15.
+//  Copyright © 2015 ryanipete. All rights reserved.
+//
+
+import XCTest
+import AmericanChronicle
+import ObjectMapper
+
+class OCRCoordinatesServiceTests: XCTestCase {
+
+    var subject: OCRCoordinatesService!
+    var manager: FakeManager!
+
+    override func setUp() {
+        super.setUp()
+        manager = FakeManager()
+        subject = OCRCoordinatesService(manager: manager)
+    }
+
+    func testThat_whenStartRequestIsCalled_withAnEmptyLCCN_itImmediatelyReturnsAnInvalidParameterError() {
+        var error: NSError? = nil
+        subject.startRequest("", date: NSDate(), edition: "", sequence: "", contextID: "") { _, err in
+            error = err as? NSError
+        }
+        XCTAssert(error?.isInvalidParameterError() ?? false)
+    }
+
+    func testThat_whenStartRequestIsCalled_withTheCorrectParameters_itStartsARequest_withTheCorrectURL() {
+        let lccn = "sn83045487"
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let date = formatter.dateFromString("1913-02-20")!
+        let edition = "ed-1"
+        let sequence = "seq-18"
+
+        subject.startRequest(lccn, date: date, edition: edition, sequence: sequence, contextID: "fake-context") { _, _ in }
+        XCTAssertEqual(manager.request_wasCalled_withURLString?.URLString, "lccn/sn83045487/1913-02-20/ed-1/seq-18/coordinates/")
+    }
+
+    func testThat_whenStartRequestIsCalled_withADuplicateRequest_itImmediatelyReturnsADuplicateRequestError() {
+        
+        subject.startRequest("sn83045487", date: NSDate(), edition: "ed-1", sequence: "seq-18", contextID: "fake-context") { _, err in }
+        var error: NSError? = nil
+        subject.startRequest("sn83045487", date: NSDate(), edition: "ed-1", sequence: "seq-18", contextID: "fake-context") { _, err in
+            error = err as? NSError
+        }
+        XCTAssert(error?.isDuplicateRequestError() ?? false)
+    }
+
+    func testThat_whenARequestSucceeds_itCallsTheCompletionHandler_withTheCoordinates() {
+        var returnedCoordinates: OCRCoordinates?
+        subject.startRequest("sn83045487", date: NSDate(), edition: "ed-1", sequence: "seq-18", contextID: "fake-context") { coordinates, _ in
+            returnedCoordinates = coordinates
+        }
+        let expectedCoordinates = OCRCoordinates(Map(mappingType: .FromJSON, JSONDictionary: [:]))
+        manager.stubbedReturnValue.finishWithResponseObject(expectedCoordinates, error: nil)
+        XCTAssertEqual(returnedCoordinates, expectedCoordinates)
+    }
+
+    func testThat_whenARequestFails_itCallsTheCompletionHandler_withTheError() {
+        var returnedError: NSError?
+        subject.startRequest("sn83045487", date: NSDate(), edition: "ed-1", sequence: "seq-18", contextID: "fake-context") { _, err in
+            returnedError = err as? NSError
+        }
+        let expectedError = NSError(code: .InvalidParameter, message: "")
+        let obj: OCRCoordinates? = nil
+        manager.stubbedReturnValue.finishWithResponseObject(obj, error: expectedError)
+        XCTAssertEqual(returnedError, expectedError)
+    }
+
+    func testThat_byTheTimeTheCompletionHandlerIsCalled_theRequestIsNotConsideredToBeInProgress() {
+        var isInProgress = true
+        let request = FakeRequest()
+        manager.stubbedReturnValue = request
+        subject.startRequest("", date: NSDate(), edition: "", sequence: "", contextID: "") { _, _ in
+            isInProgress = self.subject.isRequestInProgress("", date: NSDate(), edition: "", sequence: "", contextID: "")
+        }
+        let obj: OCRCoordinates? = nil
+        request.finishWithResponseObject(obj, error: nil)
+        XCTAssertFalse(isInProgress)
+    }
+}
