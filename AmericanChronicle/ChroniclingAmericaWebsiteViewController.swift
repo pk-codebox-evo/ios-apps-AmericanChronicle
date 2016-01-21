101a12e6fde4f85bd40d5fb50e06923fe6e66660
Ryan Peterson <ryan@carbonfive.com>
2016-01-21 12:13:49 +0000

    Added an intermediate info screen.

---
+++ AmericanChronicle/ChroniclingAmericaWebsiteViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ChroniclingAmericaWebsiteViewController.swift
+//  AmericanChronicle
+//
+//  Created by Ryan Peterson on 1/21/16.
+//  Copyright © 2016 ryanipete. All rights reserved.
+//
+
+import WebKit
+
+class ChroniclingAmericaWebsiteViewController: UIViewController {
+
+    var userDidDismiss: ((Void) -> Void)?
+
+    private let webView = WKWebView()
+
+    // MARK: UIViewController Init methods
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) not supported. Use designated initializer instead")
+    }
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Dismiss", style: .Plain, target: self, action: "dismissButtonTapped:")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.whiteColor()
+
+        view.addSubview(webView)
+        webView.snp_makeConstraints { make in
+            make.top.equalTo(0)
+            make.bottom.equalTo(0)
+            make.leading.equalTo(0)
+            make.trailing.equalTo(0)
+        }
+
+        let request = NSURLRequest(URL: NSURL(string: "http://chroniclingamerica.loc.gov/")!)
+        webView.loadRequest(request)
+    }
+
+    func dismissButtonTapped(sender: UIBarButtonItem) {
+        userDidDismiss?()
+    }
+
+}
--- AmericanChronicle/InfoViewController.swift
+++ AmericanChronicle/InfoViewController.swift
@@ -6,15 +6,36 @@
 //  Copyright © 2016 ryanipete. All rights reserved.
 //
 
-import UIKit
-import WebKit
-
 class InfoViewController: UIViewController {
 
     var userDidDismiss: ((Void) -> Void)?
 
-    private let webView = WKWebView()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Colors.darkGray
+        label.textAlignment = .Center
+        label.font = UIFont.boldSystemFontOfSize(16)
+        label.numberOfLines = 0
+        label.text = "About this app"
+        return label
+    }()
 
+    private let bodyLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Colors.darkGray
+        label.textAlignment = .Center
+        label.numberOfLines = 0
+        label.font = UIFont.systemFontOfSize(16)
+        label.text = "American Chronicle gets its data from the Chronicling America website.\n\nChronicling America is a project funded by the National Endowment for the Humanities and maintained by the Library of Congress."
+        return label
+    }()
+
+    private let websiteButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Visit chroniclingamerica.gov.loc", forState: .Normal)
+
+        return btn
+    }()
 
     // MARK: UIViewController Init methods
 
@@ -25,42 +46,43 @@ class InfoViewController: UIViewController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 
+        navigationItem.title = "About this app"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Dismiss", style: .Plain, target: self, action: "dismissButtonTapped:")
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(webView)
-        webView.snp_makeConstraints { make in
-            make.top.equalTo(0)
-            make.bottom.equalTo(0)
-            make.leading.equalTo(0)
-            make.trailing.equalTo(0)
+
+        view.backgroundColor = UIColor.whiteColor()
+
+        view.addSubview(bodyLabel)
+        bodyLabel.snp_makeConstraints { make in
+            make.top.equalTo(Measurements.verticalMargin)
+            make.leading.equalTo(Measurements.horizontalMargin)
+            make.trailing.equalTo(-Measurements.horizontalMargin)
         }
 
-        let request = NSURLRequest(URL: NSURL(string: "http://chroniclingamerica.loc.gov/")!)
-        webView.loadRequest(request)
+        websiteButton.addTarget(self, action: "websiteButtonTapped:", forControlEvents: .TouchUpInside)
+        view.addSubview(websiteButton)
+        websiteButton.snp_makeConstraints { make in
+            make.top.equalTo(bodyLabel.snp_bottom).offset(Measurements.verticalMargin * 2)
+            make.leading.equalTo(Measurements.horizontalMargin)
+            make.trailing.equalTo(-Measurements.horizontalMargin)
+            make.height.equalTo(Measurements.buttonHeight)
+        }
     }
 
     func dismissButtonTapped(sender: UIBarButtonItem) {
         userDidDismiss?()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func websiteButtonTapped(sender: UIBarButtonItem) {
+        let vc = ChroniclingAmericaWebsiteViewController()
+        vc.userDidDismiss = {
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        let nvc = UINavigationController(rootViewController: vc)
+        presentViewController(nvc, animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
