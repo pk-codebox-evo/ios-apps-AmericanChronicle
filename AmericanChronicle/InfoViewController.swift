77be25ee9ad4e76abe2931ce9c7fef7d5eb5105a
Ryan Peterson <ryan@carbonfive.com>
2016-03-13 19:50:01 +0100

    Added Zendesk

--- AmericanChronicle/InfoViewController.swift
+++ AmericanChronicle/InfoViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016 ryanipete. All rights reserved.
 //
 
+import ZendeskSDK
+
 class InfoViewController: UIViewController {
 
     var userDidDismiss: ((Void) -> Void)?
@@ -25,19 +27,43 @@ class InfoViewController: UIViewController {
         label.textColor = Colors.darkGray
         label.textAlignment = .Center
         label.numberOfLines = 0
-        label.font = Font.largeBody
-        label.text = "Chronicling America is a project funded by the National Endowment for the Humanities and maintained by the Library of Congress."
+        label.font = Font.mediumBody
+        label.text = "American Chronicle gets its data from the 'Chronicling America' website.\n\n'Chronicling America' is a project funded by the National Endowment for the Humanities and maintained by the Library of Congress."
         return label
     }()
 
     private let websiteButton: UIButton = {
         let btn = UIButton()
-        btn.titleLabel?.font = Font.largeBody
+        btn.titleLabel?.font = Font.mediumBody
         btn.setTitleColor(Colors.lightBlueBright, forState: .Normal)
         btn.setTitleColor(UIColor.whiteColor(), forState: .Highlighted)
         btn.setBackgroundImage(UIImage.imageWithFillColor(UIColor.whiteColor(), borderColor: Colors.lightBlueBright), forState: .Normal)
         btn.setBackgroundImage(UIImage.imageWithFillColor(Colors.lightBlueBright, borderColor: Colors.lightBlueBright), forState: .Highlighted)
         btn.setTitle("Visit chroniclingamerica.gov.loc", forState: .Normal)
+
+        return btn
+    }()
+
+    private let separator = UIImageView(image: UIImage.imageWithFillColor(Colors.lightBlueBright, borderColor: Colors.lightBlueBright))
+
+    private let suggestionsLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Colors.darkGray
+        label.textAlignment = .Center
+        label.numberOfLines = 0
+        label.font = Font.mediumBody
+        label.text = "Do you have a question, suggestion or complaint about the app?"
+        return label
+    }()
+
+    private let suggestionsButton: UIButton = {
+        let btn = UIButton()
+        btn.titleLabel?.font = Font.mediumBody
+        btn.setTitleColor(Colors.lightBlueBright, forState: .Normal)
+        btn.setTitleColor(UIColor.whiteColor(), forState: .Highlighted)
+        btn.setBackgroundImage(UIImage.imageWithFillColor(UIColor.whiteColor(), borderColor: Colors.lightBlueBright), forState: .Normal)
+        btn.setBackgroundImage(UIImage.imageWithFillColor(Colors.lightBlueBright, borderColor: Colors.lightBlueBright), forState: .Highlighted)
+        btn.setTitle("Send us a message", forState: .Normal)
 
         return btn
     }()
@@ -50,28 +76,35 @@ class InfoViewController: UIViewController {
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-
         navigationItem.title = "About this app"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Dismiss", style: .Plain, target: self, action: "dismissButtonTapped:")
         navigationItem.leftBarButtonItem?.setTitlePositionAdjustment(Measurements.leftBarButtonItemTitleAdjustment, forBarMetrics: .Default)
     }
 
+    func suggestionsButtonTapped(sender: UIButton) {
+        ZDKRequests.configure { account, config in
+            config.tags = ["One"]
+            config.subject = "American Chronicle"
+        }
+        ZDKRequests.showRequestCreationWithNavController(self.navigationController)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = Colors.lightBackground
 
-        view.backgroundColor = UIColor.whiteColor()
-
-        view.addSubview(titleLabel)
-        titleLabel.snp_makeConstraints { make in
-            make.top.equalTo(Measurements.verticalMargin * 3)
-            make.leading.equalTo(Measurements.horizontalMargin)
-            make.trailing.equalTo(-Measurements.horizontalMargin)
-        }
+//        view.addSubview(titleLabel)
+//        titleLabel.snp_makeConstraints { make in
+//            make.top.equalTo(Measurements.verticalMargin)
+//            make.leading.equalTo(Measurements.horizontalMargin)
+//            make.trailing.equalTo(-Measurements.horizontalMargin)
+//        }
 
         view.addSubview(bodyLabel)
         bodyLabel.snp_makeConstraints { make in
-            make.top.equalTo(titleLabel.snp_bottom).offset(Measurements.verticalMargin * 3)
+            make.top.equalTo(Measurements.verticalMargin)
+//            make.top.equalTo(titleLabel.snp_bottom).offset(Measurements.verticalSiblingSpacing)
             make.leading.equalTo(Measurements.horizontalMargin)
             make.trailing.equalTo(-Measurements.horizontalMargin)
         }
@@ -79,7 +112,31 @@ class InfoViewController: UIViewController {
         websiteButton.addTarget(self, action: "websiteButtonTapped:", forControlEvents: .TouchUpInside)
         view.addSubview(websiteButton)
         websiteButton.snp_makeConstraints { make in
-            make.top.equalTo(bodyLabel.snp_bottom).offset(Measurements.verticalMargin * 2)
+            make.top.equalTo(bodyLabel.snp_bottom).offset(Measurements.verticalSiblingSpacing * 2)
+            make.leading.equalTo(Measurements.horizontalMargin)
+            make.trailing.equalTo(-Measurements.horizontalMargin)
+            make.height.equalTo(Measurements.buttonHeight)
+        }
+
+        view.addSubview(separator)
+        separator.snp_makeConstraints { make in
+            make.top.equalTo(websiteButton.snp_bottom).offset(Measurements.verticalMargin * 2)
+            make.leading.equalTo(Measurements.horizontalMargin * 2)
+            make.trailing.equalTo(-Measurements.horizontalMargin * 2)
+            make.height.equalTo(1.0/UIScreen.mainScreen().nativeScale)
+        }
+
+        view.addSubview(suggestionsLabel)
+        suggestionsLabel.snp_makeConstraints { make in
+            make.top.equalTo(separator.snp_bottom).offset(Measurements.verticalMargin)
+            make.leading.equalTo(Measurements.horizontalMargin)
+            make.trailing.equalTo(-Measurements.horizontalMargin)
+        }
+
+        suggestionsButton.addTarget(self, action: "suggestionsButtonTapped:", forControlEvents: .TouchUpInside)
+        view.addSubview(suggestionsButton)
+        suggestionsButton.snp_makeConstraints { make in
+            make.top.equalTo(suggestionsLabel.snp_bottom).offset(Measurements.verticalSiblingSpacing * 2)
             make.leading.equalTo(Measurements.horizontalMargin)
             make.trailing.equalTo(-Measurements.horizontalMargin)
             make.height.equalTo(Measurements.buttonHeight)
