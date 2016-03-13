56600b39824f4dfb19d3de5bee561327805983dd
Ryan Peterson <ryan@carbonfive.com>
2016-03-14 00:47:56 +0100

    Simplified UI test.

--- AmericanChronicleUITests/AmericanChronicleUITests.swift
+++ AmericanChronicleUITests/AmericanChronicleUITests.swift
@@ -24,29 +24,27 @@ class AmericanChronicleUITests: XCTestCase {
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
         let app = XCUIApplication()
         app.tables.textFields["Search all Newspapers"].typeText("mark twain")
         snapshot("01Search")
-        app.tables.staticTexts["Earliest Date"].tap()
+//        app.tables.staticTexts["Earliest Date"].tap()
+//
+//        
+//        let elementsQuery = app.scrollViews.otherElements
+//        elementsQuery.buttons["Mar"].tap()
+//        app.buttons["Day"].tap()
+//        elementsQuery.buttons["17"].tap()
+//        app.buttons["Year"].tap()
 
-        
-        let elementsQuery = app.scrollViews.otherElements
-        elementsQuery.buttons["Mar"].tap()
-        app.buttons["Day"].tap()
-        elementsQuery.buttons["17"].tap()
-        app.buttons["Year"].tap()
+//        app.navigationBars["Earliest Date"].buttons["Save"].tap()
+//        app.tables.staticTexts["U.S. States"].tap()
+//        app.collectionViews.staticTexts["California"].tap()
+//        app.navigationBars["U.S. States"].buttons["Save"].tap()
 
-        app.navigationBars["Earliest Date"].buttons["Save"].tap()
-        app.tables.staticTexts["U.S. States"].tap()
-        app.collectionViews.staticTexts["California"].tap()
-        app.navigationBars["U.S. States"].buttons["Save"].tap()
-
-        app.tables.childrenMatchingType(.Cell).elementBoundByIndex(0).staticTexts["San Francisco, California"].tap()
-        snapshot("02PageZoomedOut")
+        app.tables.childrenMatchingType(.Cell).elementBoundByIndex(0).staticTexts["Washington, District of Columbia"].tap()
 
         let element = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other).element
+        snapshot("02PageZoomedOut")
         element.pinchWithScale(2.0, velocity: 1.0)
 
         snapshot("03PageZoomedIn")
